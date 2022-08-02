af3b549851335a74fb61d638f4ee4669ca942d5f
Pavol Noha <pavol.noha@gmail.com>
2022-08-02 12:43:55 +0200

    Fix rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import merge from 'deepmerge';
 import {createSpaConfig} from '@open-wc/building-rollup';
 import json from '@rollup/plugin-json';
 import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
 import summary from 'rollup-plugin-summary';
 import serve from 'rollup-plugin-serve';
 
@@ -18,11 +21,16 @@ function onwarn(warning) {
 
 export default merge(baseConfig, {
   input: './index.html',
-  onwarn,
   plugins: [
     json(),
+    commonjs(),
+    resolve({
+      browser: true,
+      preferBuiltins: true
+    }),
     summary({}),
     replace({
+      preventAssignment: true,
       ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
     process.env.NODE_ENV === 'development' &&
@@ -33,4 +41,6 @@ export default merge(baseConfig, {
         historyApiFallback: true,
       }),
   ],
+  external: ['websocket'],
+  onwarn
 });
